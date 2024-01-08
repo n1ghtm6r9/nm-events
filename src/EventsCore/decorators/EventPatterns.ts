80c8c5ddb59fa4d6a83dad7c9d5f56c779eb8054
Bogdan <n1ghtm6r9@gmail.com>
2024-01-09 00:18:05 +0200

    chore(): remove decorators

--- src/EventsCore/decorators/EventPatterns.ts
+++
@@ -1,6 +0,0 @@
-import { EventPattern } from '@nestjs/microservices';
-
-export const EventPatterns =
-  (patters: string[]): MethodDecorator =>
-  (target: object, key: string | symbol, descriptor: PropertyDescriptor) =>
-    patters.forEach(v => EventPattern(v)(target, key, descriptor));
