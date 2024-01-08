ee6842c1b7e73072187d6a493f152c421430b9c6
Bogdan <n1ghtm6r9@gmail.com>
2024-01-08 23:39:19 +0200

    fix(): bugs

--- src/NatsEvents/services/CreateClientService.ts
+++ src/NatsEvents/services/CreateClientService.ts
@@ -1,11 +1,14 @@
+import * as objHash from 'object-hash';
 import { lastValueFrom } from 'rxjs';
-import { ClientProxyFactory, Transport, NatsOptions } from '@nestjs/microservices';
+import { ClientProxyFactory, Transport, NatsOptions, ClientNats } from '@nestjs/microservices';
 import { Inject, Injectable } from '@nestjs/common';
 import { configKey, IConfig } from '@nmxjs/config';
 import type { IEventsClient } from '../../EventsClient';
 
 @Injectable()
 export class CreateClientService {
+  protected readonly clients = new Map<string, ClientNats>();
+
   constructor(@Inject(configKey) protected readonly config: IConfig) {}
 
   public async call(): Promise<IEventsClient> {
@@ -18,16 +21,22 @@ export class CreateClientService {
       },
     };
 
-    const natsClient = ClientProxyFactory.create(natsOptions);
-    await natsClient.connect();
+    const key = objHash(natsOptions);
+    let client = this.clients.get(key);
+
+    if (!client) {
+      client = ClientProxyFactory.create(natsOptions) as ClientNats;
+      this.clients.set(key, client);
+      await client.connect();
+    }
 
     return {
       options: natsOptions,
       emit: async options => {
-        await lastValueFrom(natsClient.emit(options.topic, options.data));
+        await lastValueFrom(client.emit(options.topic, options.data));
       },
       close: async () => {
-        await natsClient.close();
+        await client.close();
       },
     };
   }
